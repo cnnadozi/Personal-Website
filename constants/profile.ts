2ff9dfc58e7afb9b94c9398bbe5cacef5da7fdac
Nnadozi <chikaosro@gmail.com>
2026-06-02 00:15:00 -0700

    small changes

--- constants/profile.ts
+++ constants/profile.ts
@@ -1,10 +1,10 @@
 export const PROFILE = {
   fullName: "Chikaosolu Nnadozie",
   about:
-    "I'm a freshman at Case Western Reserve University (CWRU) double majoring in CS & Electrical Engineering. I enjoy software engineering and cross-platform mobile app development.",
+    "Sophomore at Case Western Reserve University majoring in Computer Engineering. Exploring the intersection between software and hardware applications.",
   avatar: require("../assets/images/photo.webp"),
   links: {
-    github: "https://github.com/Nnadozi",
+    github: "https://github.com/cnnadozi",
     linkedin: "https://www.linkedin.com/in/chikaosolu-nnadozie-798424279/",
     cv: "https://drive.google.com/file/d/15wH7Mt38RFFTiLRMW9CyvpEasBFhtHRw/view?usp=sharing",
     email: "mailto:chikaosro@gmail.com",
